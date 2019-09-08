a2b9b2cb7eff723982ca9ec1534ee559abc0dda6
alibek <aliibbeekk@gmail.com>
2019-09-08 10:57:01 +0300

    asdf

---
+++ EmployeeLib/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeLib.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime BornYear { get; set; }
+        public bool Gender { get; set; }
+        public bool IsMaried { get; set; }
+        public bool HasKids { get; set; }
+        public int AmountOfKids { get; set; } = 0;
+        public int UnitId { get; set; }
+        public UnitModel Unit { get; set; }
+        
+
+    }
+
+}
---
+++ EmployeeLib/Models/UnitModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeLib.Models
+{
+    public class UnitModel
+    {
+        public int UnitId { get; set; }
+        public string UnitName { get; set; }
+        public List<EmployeeModel> Employees { get; set; }
+
+    }
+}
